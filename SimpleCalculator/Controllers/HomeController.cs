f3ffdc117bc1e8c986028ab7db7e7bd26f2786b5
ZEPOY DUERO <jepoyduero@gmail.com>
2025-02-12 18:38:30 +0800

    first commit

---
+++ SimpleCalculator/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SimpleCalculator.Models;
+
+namespace SimpleCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Calculate(string expression)
+        {
+            try
+            {
+                var result = new System.Data.DataTable().Compute(expression, null);
+                return Json(new { success = true, result = result.ToString() });
+            }
+            catch (Exception)
+            {
+                return Json(new { success = false, message = "Invalid expression" });
+            }
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
